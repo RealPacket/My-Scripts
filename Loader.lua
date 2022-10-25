10797048e67d4e85904db61ab40fa7fb66ddaf05
betapacketdlldink.nodllforyou <107498533+RealPacket@users.noreply.github.com>
2022-10-25 07:19:20 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,7 +5,7 @@ local games = {
     [{7560156054}] = "https://raw.githubusercontent.com/https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/ClickingGames/Clicker%20Simulator/Clicker%20Simulator.lua/My-Scripts/main/Clicker%20Simulator.lua",
     [{9912491609}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/How%20Far%20can%20You%20Fly/Script.lua",
     [{9498006165}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/ClickingGames/Tapping%20Simulator/TappingScript.lua",
-    [{3851622790}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/BreakIn/BreakInHubLoader.lua"
+    [{3851622790}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/BreakIn/BreakInHubLoader.lua", -- Why do I need a extra comma at the end bru
 }
 
 for ids, url in next, games do
