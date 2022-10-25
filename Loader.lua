f7569c1cb09810d135751e37243030472d994169
betapacketdlldink.nodllforyou <107498533+RealPacket@users.noreply.github.com>
2022-10-25 07:18:07 -0500

    Use break in hub loader for break in story

--- Loader.lua
+++ Loader.lua
@@ -5,12 +5,7 @@ local games = {
     [{7560156054}] = "https://raw.githubusercontent.com/https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/ClickingGames/Clicker%20Simulator/Clicker%20Simulator.lua/My-Scripts/main/Clicker%20Simulator.lua",
     [{9912491609}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/How%20Far%20can%20You%20Fly/Script.lua",
     [{9498006165}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/ClickingGames/Tapping%20Simulator/TappingScript.lua",
-                      --[[
-                          TODO: 
-                            Change this to the loader when I release the loader...
-                            Loader coming soon (will use discord UI Lib as I think it is the best for loaders and some people use it)
-                        ]]
-    [{3851622790}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/BreakIn/KavoUI/BreakInStoryKavo.lua"
+    [{3851622790}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/BreakIn/BreakInHubLoader.lua"
 }
 
 for ids, url in next, games do
