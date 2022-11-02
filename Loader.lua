5dd72dea10380ca6d79ce5982ef9a486ca871a2f
betapacketdlldink.nodllforyou <107498533+RealPacket@users.noreply.github.com>
2022-11-02 18:15:36 -0500

    omg I made my shit work

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,9 @@
--- Skidded lololololol
 local games = {
-    -- commas added cause loader prob will work if I do.
     [{8884433153}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/Collect%20All%20Pets/Script.lua",
     [{7560156054}] = "https://raw.githubusercontent.com/https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/ClickingGames/Clicker%20Simulator/Clicker%20Simulator.lua/My-Scripts/main/Clicker%20Simulator.lua",
     [{9912491609}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/How%20Far%20can%20You%20Fly/Script.lua",
     [{9498006165}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/ClickingGames/Tapping%20Simulator/TappingScript.lua",
-    [{3851622790}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/BreakIn/BreakInHubLoader.lua"
+    [{3851622790}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/BreakIn/BreakInHubLoader.lua",
 }
 
 for ids, url in next, games do
