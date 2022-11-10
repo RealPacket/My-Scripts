872a8679c085a2da1abb37a8846fa95c6d39a54d
betapacketdlldink.nodllforyou <107498533+RealPacket@users.noreply.github.com>
2022-11-10 15:18:20 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@
 local games = {
     [{8884433153}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/Collect%20All%20Pets/Script.lua",
     [{7560156054}] = "https://raw.githubusercontent.com/https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/ClickingGames/Clicker%20Simulator/Clicker%20Simulator.lua/My-Scripts/main/Clicker%20Simulator.lua",
-    [{9912491609}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/How%20Far%20can%20You%20Fly/Script.lua",
+    [{9912491609}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/How%20Far%20can%20You%20Fly/Orion%20Library/Script.lua",
     [{9498006165}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/ClickingGames/Tapping%20Simulator/TappingScript.lua",
     [{3851622790}] = "https://raw.githubusercontent.com/RealPacket/My-Scripts/main/Games/BreakIn/BreakInHubLoader.lua",
 }
