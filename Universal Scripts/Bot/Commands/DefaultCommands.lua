e7a1f7e1de122bb9b7ef79ac8be6db58f7e3272a
betapacketdlldink.nodllforyou <107498533+RealPacket@users.noreply.github.com>
2024-02-11 13:12:00 -0600

    fix targetstrafe
    forgot it takes a vector3 not a number

--- Universal Scripts/Bot/Commands/DefaultCommands.lua
+++ Universal Scripts/Bot/Commands/DefaultCommands.lua
@@ -449,7 +449,7 @@ runFn(function()
 				and CFrame.lookAt(pos, Vector3.new(targetPosition.X, Character:GetPivot().Y, targetPosition.Z))
 			or CFrame.new(pos) * CFrame.Angles(Character:GetPivot():ToEulerAnglesXYZ())
 		-- try to not teleport in air so it doesn't fall
-		if not workspace:Raycast(newPosition.Position, 200) then
+		if not workspace:Raycast(newPosition.Position, Vector3.new(0, 200, 0)) then
 			return
 		end
 		-- Move to the new position
