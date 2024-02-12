fc21ef2c3f8e305b2ede85bd89428175ee7bca9b
betapacketdlldink.nodllforyou <107498533+RealPacket@users.noreply.github.com>
2024-02-11 18:55:15 -0600

    Change BATCH_SIZE to 5

--- Universal Scripts/Bot/Commands/DefaultCommands.lua
+++ Universal Scripts/Bot/Commands/DefaultCommands.lua
@@ -304,10 +304,10 @@ runFn(function()
 			table.insert(names, name)
 		end
 		Betabot.Utils.Chat(("Commands (%d):"):format(#names))
-		local BATCH_SIZE = 9
+		local BATCH_SIZE = 5
 		for i = 1, #names, BATCH_SIZE do
 			print("i=", i)
-			Betabot.Utils.Chat(table.concat(names, ", ", i, i - BATCH_SIZE))
+			Betabot.Utils.Chat(table.concat(names, ", ", i, i + BATCH_SIZE))
 		end
 		task.wait(1)
 		listing = false
