dee16fb0ff69e9f318e63ecb4411c15e183db4b9
betapacketdlldink.nodllforyou <107498533+RealPacket@users.noreply.github.com>
2024-02-16 14:46:04 -0600

    disable testing in loadstring

--- Universal Scripts/Bot/loadstring.lua
+++ Universal Scripts/Bot/loadstring.lua
@@ -1,11 +1,8 @@
-local testing = true
+local testing = false
 local file = testing and "Bot-testing.lua" or "Bot.lua"
 loadstring(
 	game:HttpGet(
-		"https://raw.githubusercontent.com/RealPacket/My-Scripts/main/"
-			.. "Universal%20Scripts"
-			.. "/Bot/"
-			.. file
+		"https://raw.githubusercontent.com/RealPacket/My-Scripts/main/" .. "Universal%20Scripts" .. "/Bot/" .. file
 	),
 	"Betabot"
 )()
