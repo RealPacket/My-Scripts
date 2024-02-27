319bd88400122f7ccc8a8011566ba41ee1abd4a2
betapacketdlldink.nodllforyou <107498533+RealPacket@users.noreply.github.com>
2024-02-27 16:02:33 -0600

    Update ProxPrompt delay script
    this change is all you should care about:
    [~] rename (shared.)NoProxPromptWait to ProxPromptSettings
    
    the rest are just internal changes.
    
    [-] Removed useless variables (moddedProximityPrompts)
    [-] Removed config variable (Enabled)
         It was never implemented anyways.
         Not like it was going to be.
    [~] renamed noDelay function to handle and make it local
    [~] add guard clause for less indenting
    [~] renamed SettingsHolder variable to
    [+] added wrapper function around handle named `connectionHandler`

--- Universal Scripts/Custom ProxPrompt Delay.lua
+++ Universal Scripts/Custom ProxPrompt Delay.lua
@@ -17,28 +17,32 @@
 -- I cleaned it up.
 -- You can find the documentation of the ProximityPrompt class at https://create.roblox.com/docs/reference/engine/classes/ProximityPrompt
 
-shared.NoProxPromptWait = {
+shared.ProxPromptSettings = {
 	Config = {
 		Delay = 0, -- anything above 0 isn't instant.
-		Enabled = true, -- if the mod should be enabled.
 	},
 }
-local SettingsHolder = shared.NoProxPromptWait
-local moddedProximityPrompts = {}
+local settings = shared.ProxPromptSettings
+---@param ProxPrompt ProximityPrompt
+local function handle(ProxPrompt)
+	ProxPrompt.HoldDuration = settings.Config.Delay or 0 -- Current config delay or instant.
+end
+
 -- if you were trying to get only the ones parented to the workspace, and not some folder in the workspace, you'd change this to workspace:GetChildren()
 for _, ProxPrompt in workspace:GetDescendants() do
 	-- Make sure that there's only Proximity Prompts,
 	-- if we allowed every type of class, we'd get a error if there would be even just 1 thing that isn't a proximity prompt.
-	if ProxPrompt:IsA("ProximityPrompt") then
-		-- Modify the wait delay
-		ProxPrompt.HoldDuration = SettingsHolder.Config.Delay or 0 -- Current config's delay or instant.
-		table.insert(moddedProximityPrompts, ProxPrompt)
+	if not ProxPrompt:IsA("ProximityPrompt") then
+		return
 	end
+	handle(ProxPrompt)
 end
-function noDelay(ProxPrompt)
-	if ProxPrompt:IsA("ProximityPrompt") then
-		ProxPrompt.HoldDuration = SettingsHolder.Config.Delay or 0 -- Current config delay or instant.
-		table.insert(moddedProximityPrompts, ProxPrompt)
+---wraps around handle function to check the instance.
+---@param inst Instance
+local function connectionHandler(inst)
+	if not inst:IsA("ProximityPrompt") then
+		return
 	end
+	return handle(inst)
 end
-workspace.DescendantAdded:Connect(noDelay)
+workspace.DescendantAdded:Connect(connectionHandler)
